87db5aab1ce0026f36cdc02f77492da70a1718bf
Pablo Marti <startpablo@gmail.com>
2016-12-10 21:44:38 -0600

    Add mail support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { :host => 'ibvn2.org' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+  
+  ActionMailer::Base.smtp_settings = {
+    :port           => 587,
+    :address        => "smtp.mailgun.org",
+    :user_name      => 'postmaster@ibvn2.org',
+    :password       => '316Futuro',
+    :authentication => :plain,
+  }
 end
---
+++ config/initializers/setup_mail.rb
@@ -0,0 +1,4 @@
+# ActionMailer::Base.smtp_settings = {
+#   :domain         => 'ibvn2.org',
+#   :enable_starttls_auto => :false
+# }
